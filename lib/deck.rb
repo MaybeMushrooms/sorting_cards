bce0ba959efc618492a8a2c8f2094b8ad4e711a9
maybemushrooms <thecraftedgem@tuta.io>
2018-07-04 21:44:36 -0600

    Sorting Working

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,8 @@
+require './lib/card'
+
 class Deck
-  attr_reader :cards
+  attr_reader :cards,
+              :sort
   def initialize(cards)
     @cards = cards
     @count = count
@@ -7,5 +10,19 @@ class Deck
 
   def count
     cards.length
+  end
+
+  def sort
+    swapped = true
+    while swapped do
+      swapped = false
+      (count - 1).times do |card|
+        if @cards[card].card_total > @cards[card + 1].card_total
+          @cards[card], @cards[card + 1] = @cards[card + 1], @cards[card]
+          swapped = true
+        end
+      end
+    end
+    @cards
   end
 end
